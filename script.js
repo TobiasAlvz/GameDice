ee7666fa0e786801a0b02a2101d8a5a773673794
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2023-04-13 19:41:42 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function rolarDado() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function atualizarTexto(elemento, texto) {
+  elemento.textContent = texto;
+}
+
+let numeroJogador1 = rolarDado();
+let numeroJogador2 = rolarDado();
+
+let img1 = document.getElementById("img1");
+img1.setAttribute("src", `img/dice${numeroJogador1}.png`);
+
+let img2 = document.getElementById("img2");
+img2.setAttribute("src", `img/dice${numeroJogador2}.png`);
+
+let resultado;
+
+if (numeroJogador1 > numeroJogador2) {
+  resultado = "🏆Jogador 1 ganhou!";
+} else if (numeroJogador2 > numeroJogador1) {
+  resultado = "🏆Jogador 2 ganhou!";
+} else {
+  resultado = "Empate!";
+}
+
+let h1 = document.querySelector("h1");
+atualizarTexto(h1, resultado);
